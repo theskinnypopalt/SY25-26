51b8240e09d431a99d08f2024ebee97eb7e198f1
Beckett <bmste396@learner.dvusd.org>
2025-08-21 11:04:41 -0700

    first project
    astro signs project

---
+++ multi_project-real/zodiac.java
@@ -0,0 +1,85 @@
+
+/**
+ * Write a description of class zodiac here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class zodiac
+{
+  public static void main(String[] args)
+  {
+      Scanner input = new Scanner(System.in);
+     System.out.println("Enter your sign:");
+     String sign = input.nextLine();
+    if (sign .equals("Aries"))
+    {
+        System.out.println("Your sign is: ♈");
+        System.out.println("Your birthday is: March 21 to April 19");
+    }
+    if (sign .equals("Taurus"))
+    {
+        System.out.println("Your sign is: ♉");
+        System.out.println("Your birthday is: April 20 to May 20");
+    }
+    if (sign .equals("Gemini"))
+    {
+        System.out.println("Your sign is: ♊");
+        System.out.println("Your birthday is: May 21 to June 20");
+    }
+    if (sign .equals("Cancer"))
+    {
+        System.out.println("Your sign is: ♋");
+        System.out.println("Your birthday is: June 21 to July 22");
+    }
+    if (sign .equals("Leo"))
+    {
+        System.out.println("Your sign is: ♌");
+        System.out.println("Your birthday is: July 23 to August 22");
+    }
+    if (sign .equals("Virgo"))
+    {
+        System.out.println("Your sign is: ♍");
+        System.out.println("Your birthday is: August 23 to Sept. 22");
+    }
+    if (sign .equals("Libra"))
+    {
+        System.out.println("Your sign is: ♎");
+        System.out.println("Your birthday is: Sept. 23 to Oct. 22");
+    }
+    if (sign .equals("Scorpio"))
+    {
+        System.out.println("Your sign is: ♏");
+        System.out.println("Your birthday is: Oct 23 to Nov. 21");
+    }
+    if (sign .equals("Sagittarius"))
+    {
+        System.out.println("Your sign is: ♐");
+        System.out.println("Your birthday is: Nov. 22 to Dec 21");
+    }
+    if (sign .equals("Capricon"))
+    {
+        System.out.println("Your sign is: ♑");
+        System.out.println("Your birthday is: Dec. 22 to Jan 19");
+    }
+    if (sign .equals("Aquarius"))
+    {
+        System.out.println("Your sign is: ♒");
+        System.out.println("Your birthday is: Jan 20 to Feb 18");
+    }
+    if (sign .equals("Pisces"))
+    {
+        System.out.println("Your sign is: ♓");
+        System.out.println("Your birthday is: Feb 19 to March 20");
+    }
+    
+    }
+    }
+    
+    
+    
+    
+    
+    
+
